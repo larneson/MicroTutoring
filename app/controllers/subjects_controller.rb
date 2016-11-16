0bd8386f7f688183250aae84f549aaa750b280c5
nazgus <gus@deleon.org.mx>
2016-11-16 13:11:22 -0800

    Add Subject view
    Includes name of subject being viewed and a list of associated courses
    which displays the number and description for each.

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,2 +1,6 @@
 class SubjectsController < ApplicationController
+
+    def show
+        @subject = Subject.find(params[:id])
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161116204911) do
+ActiveRecord::Schema.define(version: 20161116211007) do
 
   create_table "courses", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "number"
+    t.text     "description"
   end
 
   create_table "subjects", force: true do |t|
